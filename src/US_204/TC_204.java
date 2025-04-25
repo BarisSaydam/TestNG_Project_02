62907dd8a73facbf5aae84f8910e3e359c0826ba
HP <gamze.batmazzz@gmail.com>
2025-04-25 20:39:10 +0100

    Test basariyla tamamlanmistir.

---
+++ src/US_204/TC_204.java
@@ -0,0 +1,32 @@
+package US_204;
+
+import Utility.BaseDriver;
+import Utility.BaseDriverParameter;
+import Utility.MyFunc;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_204 extends BaseDriverParameter {
+    @Test
+    public void Test1(){
+        TC_204_Elements elements=new TC_204_Elements(driver);
+        driver.get("https://techno.study/");
+
+
+        elements.blogs.click();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,500)");
+
+        wait.until(ExpectedConditions.visibilityOf(elements.blogyazisi));
+
+        elements.blogyazisi.click();
+
+        Assert.assertTrue(elements.dogrulama.getText().contains("Explore how manual testing ensures comprehensive test coverage, identifies complex bugs, and enhances software quality and reliability."));
+
+
+    }
+
+}
---
+++ src/US_204/TC_204_Elements.java
@@ -0,0 +1,22 @@
+package US_204;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC_204_Elements {
+    public TC_204_Elements(WebDriver driver){PageFactory.initElements(driver,this);}
+
+    @FindBy(xpath = "//*[text()='Blogs'][1]")
+    public WebElement blogs;
+
+    @FindBy(xpath = "//*[text()='Importance of Manual Testing  in the Modern SDLC']")
+    public WebElement blogyazisi;
+
+    @FindBy(xpath = "//*[@class='t986__descr t-descr t-descr_lg']")
+    public WebElement dogrulama;
+
+
+
+}
